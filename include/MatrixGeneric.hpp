71a2d4fe3b01e383a8ccbbd52348e9d6acc7e830
dmsukhikh <dimasssuchich@mail.ru>
2025-03-28 13:17:37 +0300

    Constructors and destructor for MatrixGeneric

--- include/MatrixGeneric.hpp
+++ include/MatrixGeneric.hpp
@@ -1,11 +1,35 @@
-#ifndef __MATRIX
-#define __MATRIX
+#ifndef __MATRIX_GENERIC
+#define __MATRIX_GENERIC
+
+#include <vector>
+#include <algorithm>
+#include <stdint.h>
 
 template <typename T> class MatrixGeneric
 {
   public:
-  private:
+    explicit MatrixGeneric() = default;
+    explicit MatrixGeneric(uint32_t height, uint32_t width)
+        : _height(height), _width(width)
+    {
+        _data.resize(_height);
+        for (auto &raw: _data)
+        {
+            raw.resize(_width);
+        }
+    }
 
+    MatrixGeneric(const MatrixGeneric &) = default;
+    MatrixGeneric &operator=(const MatrixGeneric &) = default;
+
+    MatrixGeneric(MatrixGeneric &&) = default;
+    MatrixGeneric &operator=(MatrixGeneric &&) = default;
+
+    virtual ~MatrixGeneric() = default;
+
+  private:
+    uint32_t _height, _width;
+    std::vector<std::vector<T>> _data;
 };
 
 #endif
