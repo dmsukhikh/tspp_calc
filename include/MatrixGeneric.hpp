7064a1008e0d3fb14f8550cba630bf39500ac6dc
Дмитрий Сухих <dimasssuchich@mail.ru>
2025-03-30 20:00:08 +0000

    First release v1.0

---
+++ include/MatrixGeneric.hpp
@@ -0,0 +1,312 @@
+#ifndef __MATRIX_GENERIC
+#define __MATRIX_GENERIC
+
+#include <cstdint>
+#include <initializer_list>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include "MatrixOperation.hpp"
+#include "Exceptions.hpp"
+
+template <typename T> class MatrixGeneric
+{
+  public:
+    explicit MatrixGeneric() = default;
+    explicit MatrixGeneric(uint32_t height, uint32_t width)
+        : _height(height), _width(width)
+    {
+        if ((_height == 0 && _width != 0) || (_width == 0 && _height != 0))
+        {
+            throw matrix_initialization_error(
+                "Only both height and width can be zero");
+        }
+        _data.resize(_height * _width);
+    }
+
+    MatrixGeneric(const MatrixGeneric &) = default;
+    MatrixGeneric &operator=(const MatrixGeneric &) = default;
+
+    MatrixGeneric(const std::initializer_list<std::initializer_list<T>> &nums)
+    {
+        if (nums.size() == 0)
+            return ;
+
+        auto width = nums.begin()->size();
+        auto height = nums.size();
+        auto isValidList = std::all_of(nums.begin(), nums.end(),
+                                       [width](const std::initializer_list<T> &raw)
+                                       { return raw.size() == width; });
+        if (!isValidList)
+            throw matrix_initialization_error("Invalid initializer list");
+        
+        _width = width;
+        _height = height;
+        
+        for (auto &raw: nums)
+        {
+            for (auto &i: raw)
+            {
+                _data.push_back(i);
+            }
+        }
+    }
+
+    static MatrixGeneric eye(uint32_t size)
+    {
+        MatrixGeneric<T> out(size, size);
+        for (uint32_t i = 0; i < size; ++i)
+        {
+            out.get(i, i) = T(1);
+        }
+        return out;
+    }
+
+    MatrixGeneric(MatrixGeneric &&other)
+    {
+        *this = std::move(other);
+    };
+
+    MatrixGeneric &operator=(MatrixGeneric &&other) 
+    {
+        if (&other != this)
+        {
+            _height = other._height;
+            _width = other._width;
+            _data = std::move(other._data);
+            other._height = 0;
+            other._width = 0;
+        }
+        return *this;
+    }
+
+    ~MatrixGeneric() = default;
+
+    T& get(uint32_t x, uint32_t y)
+    {
+        return const_cast<T&>(
+            const_cast<const MatrixGeneric<T> *>(this)->get(x, y)
+                );
+    }
+
+    const T& get(uint32_t x, uint32_t y) const
+    {
+        auto idx = x * _width + y;
+        if (idx >= _width * _height)
+        {
+            throw matrix_bad_access("Indexing is out of range");
+        }
+
+        return _data[x*_width + y];
+    }
+
+    uint32_t height() const noexcept
+    {
+        return _height;
+    }
+
+    uint32_t width() const noexcept
+    {
+        return _width;
+    }
+
+    template <typename A, typename B>
+    friend MatrixGeneric<AddType<A, B>> operator+(const MatrixGeneric<A> &a,
+                                                  const MatrixGeneric<B> &b);
+
+    template <typename A, typename B>
+    friend MatrixGeneric<SubsType<A, B>> operator-(const MatrixGeneric<A> &a,
+                                                   const MatrixGeneric<B> &b);
+
+    template <typename A, typename B>
+    friend MatrixGeneric<AddType<MulType<A, B>, MulType<A, B>>>
+    operator*(const MatrixGeneric<A> &a, const MatrixGeneric<B> &b);
+
+    template <typename G, typename Scalar>
+    friend MatrixGeneric<MulType<Scalar, G>>
+    operator*(Scalar scalar, const MatrixGeneric<G> &a);
+
+    template <typename A, typename B>
+    friend MatrixGeneric<MulType<A, DivType<B, float>>>
+    operator/(const MatrixGeneric<A> &a, const MatrixGeneric<B> &b);
+
+    template <typename A, typename B>
+    friend bool operator==(const MatrixGeneric<A> &a, const MatrixGeneric<B> &b);
+
+    template <typename A, typename B>
+    friend bool operator!=(const MatrixGeneric<A> &a, const MatrixGeneric<B> &b);
+
+    T det() const
+    {
+        if (_height != _width)
+            throw matrix_bad_det("Matrix isn't square"); 
+
+        if (_height == 0 && _width == 0)
+            return 1; 
+
+        return _gauss().second;    
+    }
+
+
+    MatrixGeneric transpose()
+    {
+        std::vector<T> _new_data(_width * _height);
+        for (uint32_t i = 0; i < _height; ++i)
+        {
+            for (uint32_t j = 0; j < _width; ++j)
+            {
+                _new_data[j*_height + i] = _data[i*_width + j];
+            }
+        }
+
+        MatrixGeneric<T> out(_width, _height);
+        out._data = std::move(_new_data);
+        return out;
+    }
+
+    MatrixGeneric<DivType<T, float>> inverse() const
+    {
+        if (_height != _width)
+            throw matrix_bad_inverse("Matrix isn't square");
+
+        MatrixGeneric<DivType<T, float>> out(_height, _width);
+
+        auto delta = det();
+        if (delta == 0)
+            throw matrix_bad_inverse("Matrix determinant equals zero"); 
+
+        for (uint32_t i = 0; i < _height; ++i)
+        {
+            for (uint32_t j = 0; j < _width; ++j)
+            {
+                out.get(i, j) = ((i + j) % 2 ? -1 : 1) * _cofactor(i, j).det();
+            }
+        }
+
+        out = (1.f / delta) * out.transpose();
+        return out;
+    }
+
+    MatrixGeneric pow(uint32_t power)
+    {
+        if (_height != _width)
+            throw matrix_bad_pow("Matrix isn't square");
+
+        if (power == 0)
+            return MatrixGeneric<T>::eye(_height);
+
+        MatrixGeneric<T> out(*this);
+
+        while (power > 1)
+        {
+            out = out * *this;
+            --power;
+        }
+        return out;
+    }
+
+    uint32_t rk() const
+    {
+        return _gauss().first;
+    }
+
+  private:
+    uint32_t _height{0}, _width{0};
+    std::vector<T> _data;
+
+    MatrixGeneric _cofactor(uint32_t i, uint32_t j) const
+    {
+        if (i >= _height)
+            throw matrix_bad_access("i is out of range");
+        if (j >= _width)
+            throw matrix_bad_access("j is out of range");
+
+        MatrixGeneric out = *this;
+
+        auto cnt = 0;
+        auto deleter = [&cnt, i, j, this](const T&)
+        {
+            auto pred = cnt / _width == i || cnt % _width == j;
+            ++cnt;
+            return pred;
+        };
+
+        auto to_del =
+            std::remove_if(out._data.begin(), out._data.end(), deleter);
+        out._data.erase(to_del, out._data.end());
+        out._height--;
+        out._width--;
+
+        return out;
+    }
+
+    std::pair<uint32_t, T> _gauss() const
+    {
+        // Метод Гаусса приведения матрицы к верхнетреугольному виду. 1 - ранг,
+        // 2 - определитель
+
+        char sign = 1;
+
+        MatrixGeneric<DivType<float, T>> _copy(_height, _width);
+        for (uint32_t i = 0; i < _width * _height; ++i)
+        {
+            _copy.get(i/_width, i%_width) = _data[i];
+        }
+        uint32_t rank = std::min(_height, _width);
+
+        for (uint32_t row = 0; row < rank; row++)
+        {
+            if (_copy.get(row, row) != 0)
+            {
+                for (uint32_t targetRow = 0; targetRow < _height; targetRow++)
+                {
+                    if (targetRow != row)
+                    {
+                        auto mult = (float)_copy.get(targetRow, row) /
+                                    _copy.get(row, row);
+                        for (uint32_t i = 0; i < rank; i++)
+                            _copy.get(targetRow, i) -= mult * _copy.get(row, i);
+                    }
+                }
+            }
+            else
+            {
+                bool reduce = true;
+                for (uint32_t i = row + 1; i < _height; i++)
+                {
+                    if (_copy.get(i, row) != 0)
+                    {
+                        for (uint32_t targetRow = 0; targetRow < rank;
+                             targetRow++)
+                        {
+                            std::swap(_copy.get(row, targetRow),
+                                      _copy.get(i, targetRow));
+                        }
+                        reduce = false;
+                        sign *= -1;
+                        break;
+                    }
+                }
+
+                if (reduce)
+                {
+                    rank--;
+                    sign = 0;
+                    for (uint32_t i = 0; i < _height; i ++)
+                        _copy.get(i, row) = _copy.get(i, rank);
+                }
+                row--;
+            }
+        }
+        
+        T absdet = (_width == 0 && _height == 0) ? 0 : 1;
+        for (uint32_t i = 0; i < std::min(_width, _height); ++i)
+        {
+            absdet *= _copy.get(i, i);
+        }
+
+        return {rank, absdet*sign};
+    }
+};
+
+#endif
---
+++ test/test_funcs.cpp
@@ -0,0 +1,113 @@
+#include <functional>
+#include <gtest/gtest.h>
+#include "Exceptions.hpp"
+#include "MatrixGeneric.hpp"
+
+// Test suite для тестирования методов MatrixGeneric
+
+TEST(TestFuncs, TestDet)
+{
+    // Поиск определителя
+    
+    MatrixGeneric a = {{1, 4, 0, 12},{-1, -2, 1, 1},{0, 1, 1, -1},{7, 0, 1, 2}};
+    EXPECT_NEAR(a.det(), 325, 10e-6);
+    EXPECT_EQ((3 * a).det(), 325 * 81);
+
+    MatrixGeneric b = {{1, 7, 0},{-1, -2, 0},{1, 3, 5}};
+    EXPECT_NEAR(b.det(), 25, 10e-6);
+
+    MatrixGeneric rect = {{1, 3, 0, 2}, {1, 0, 1, 2}, {1, 2, 3, 1}};
+    EXPECT_THROW(rect.det(), matrix_bad_det);
+
+    MatrixGeneric<int> empty;
+    EXPECT_NEAR(empty.det(), 1, 10e-6);
+
+    MatrixGeneric diag = {{2, 3, 2},{0, 1, 2},{0, 0, 4}};
+    EXPECT_NEAR(diag.det(), 2*1*4, 10e-6);
+
+    MatrixGeneric zerodet = {{1, 1, 2, 2},{0, 1, 0, 0},{0, 0, 1, 0},{1, 3, 2, 2}};
+    EXPECT_NEAR(zerodet.det(), 0, 10e-6);
+    EXPECT_NEAR(MatrixGeneric<int>::eye(4).det(), 1, 10e-6);
+}
+
+TEST(TestFuncs, TestRk)
+{
+    EXPECT_EQ(MatrixGeneric<int>::eye(5).rk(), 5);
+
+    MatrixGeneric<int> a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 8}};
+    EXPECT_EQ(a.rk(), 3);
+
+    MatrixGeneric b = {{1, 4, 0, 12},{-1, -2, 1, 1},{0, 1, 1, -1},{7, 0, 1, 2}};
+    EXPECT_EQ(b.rk(), 4);
+
+    MatrixGeneric c = {{1, 0, 0, 0, 12}, {0, 1, 0, 11, -11}, {0, 0, 1, -2, -5}};
+    EXPECT_EQ(c.rk(), 3);
+    
+    MatrixGeneric d = {{1, 0},{0, 1},{1, 1},{0, 0},{1, 1}}; 
+    EXPECT_EQ(d.rk(), 2);
+
+    MatrixGeneric e = {{1, 1, 1}, {0, 1, 0}, {2, 5, 2}};
+    EXPECT_EQ(e.rk(), 2);
+    EXPECT_EQ(MatrixGeneric<int>(3, 3).rk(), 0);
+    EXPECT_EQ(MatrixGeneric<int>{}.rk(), 0);
+}
+
+TEST(TestFuncs, TestEye)
+{
+    // Тест, проверяющий корректность создания единичной матрицы
+    auto E = MatrixGeneric<int>::eye(20);
+    for (int i = 0; i < 20; ++i)
+    {
+        for (int j = 0; j < 20; ++j)
+        {
+            if (i == j) EXPECT_EQ(E.get(i, j), 1);
+            else        EXPECT_EQ(E.get(i, j), 0);
+        }
+    }
+    auto smallE = MatrixGeneric<int>::eye(1);
+    EXPECT_EQ(smallE, MatrixGeneric<int>{{1}});
+}
+
+TEST(TestFuncs, TestTranspose)
+{
+    MatrixGeneric a = {{1, 4, 3, 2}, {1, 4, 5, 6}, {0, 1, 4, 0}};
+    auto b = a.transpose();
+
+    EXPECT_TRUE(a.width() == b.height() && a.height() == b.width());
+    for (int i = 0; i < a.height(); ++i)
+    {
+        for (int j = 0; j < a.width(); ++j)
+        {
+            EXPECT_EQ(a.get(i, j), b.get(j, i));
+        }
+    }
+}
+
+TEST(TestFuncs, TestInverse)
+{
+    MatrixGeneric bad_1 = {{1, 2, 3, 4, 5}},
+                  bad_2 = {{1, 2},  {2, 4}};
+    EXPECT_THROW(bad_1.inverse(), matrix_bad_inverse);
+    EXPECT_THROW(bad_2.inverse(), matrix_bad_inverse);
+
+    MatrixGeneric good_1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 8}},
+                  good_2 = {{1, 4, 0, 12},{-1, -2, 1, 1},{0, 1, 1, -1},{7, 0, 1, 2}};
+    
+    MatrixGeneric out_1 = good_1 * good_1.inverse(),
+                  out_2 = good_2 * good_2.inverse();
+
+    for (auto &testCase :
+         {std::reference_wrapper(out_1), std::reference_wrapper(out_2)})
+    {
+        for (int i = 0; i < testCase.get().height(); ++i)
+        {
+            for (int j = 0; j < testCase.get().width(); ++j)
+            {
+                if (i == j)
+                    EXPECT_NEAR(testCase.get().get(i, j), 1, 10e-6);
+                else
+                    EXPECT_NEAR(testCase.get().get(i, j), 0, 10e-6);
+            }
+        }
+    }
+}
